4a45ad567bac48d671009276bc42fa4f4d7c08e6
MiguelFrBarreto <miguelfrbarreto@gmail.com>
2025-12-13 17:40:39 -0300

    Base classes and enum

---
+++ todo/src/main/java/com/example/todo/enums/Status.java
@@ -0,0 +1,7 @@
+package com.example.todo.enums;
+
+public enum Status {
+    PENDING,
+    COMPLETED,
+    EXPIRED;
+}
---
+++ todo/src/main/java/com/example/todo/task/Task.java
@@ -0,0 +1,46 @@
+package com.example.todo.task;
+
+import java.time.Instant;
+
+import com.example.todo.enums.Status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    private Instant creationDate;
+    private Instant expirationDate;
+    private Instant completionDate;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Task(String title, String description, Instant expirationDate) {
+        this.title = title;
+        this.description = description;
+        this.creationDate = Instant.now();
+        this.expirationDate = expirationDate;
+        this.status = Status.PENDING;
+    }
+}
